546056dc975832ec2ae03aa9b845c1aa6a932446
Steve Richert <steve.richert@gmail.com>
2014-01-23 12:24:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fireside::Application.config.secret_key_base = '7a9b10f0e5d333159565407c6d985186e4ce6beaf1fdc0922812fe7c1e1cf1f7c08ac8214d4438268922acf7c75969fae696a11754cc578d3b2cccf79de13813'
