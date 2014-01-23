18f8fef9019ca229821c211b4673266c433a6617
Steve Richert <steve.richert@gmail.com>
2014-01-23 13:02:35 -0500

    Install RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+ENV["RAILS_ENV"] ||= "test"
+
+require File.expand_path("../../config/environment", __FILE__)
+
+require "rspec/rails"
+require "rspec/autorun"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
