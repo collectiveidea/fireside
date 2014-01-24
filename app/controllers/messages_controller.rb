3a9d85b5f4fa08f0959bf1a7b1bb99ed0a368eb9
Steve Richert <steve.richert@gmail.com>
2014-01-24 09:59:20 -0500

    Nest messages underneath rooms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,20 @@
 class MessagesController < ApplicationController
+  before_action :load_room
+
   def index
-    @messages = Message.old_to_new
+    @messages = @room.messages.old_to_new
   end
 
   def create
-    @message = Message.create!(message_params)
+    @message = @room.messages.create!(message_params)
     render :show, status: :created
   end
 
   private
+
+  def load_room
+    @room = Room.find(params[:room_id])
+  end
 
   def message_params
     params.require(:message).permit(:body)
