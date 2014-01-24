853f612a2262e34863ad6512fd7799b45a3ce685
Steve Richert <steve.richert@gmail.com>
2014-01-24 09:58:03 -0500

    Associate messages with a specific room

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124105809) do
+ActiveRecord::Schema.define(version: 20140124125651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20140124105809) do
   create_table "messages", force: true do |t|
     t.text     "body"
     t.datetime "created_at"
+    t.integer  "room_id"
   end
+
+  add_index "messages", ["room_id"], name: "index_messages_on_room_id", using: :btree
 
   create_table "rooms", force: true do |t|
     t.string   "name"
