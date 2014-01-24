048cc02f6ac75367590ac4ab58e1ef9936017824
Steve Richert <steve.richert@gmail.com>
2014-01-24 10:13:20 -0500

    Add the rooms#show endpoint

---
+++ app/views/rooms/show.json.jbuilder
@@ -0,0 +1,12 @@
+json.room do
+  json.active_token_value @room.active_token_value
+  json.created_at @room.created_at
+  json.full @room.full?
+  json.id @room.id
+  json.locked @room.locked?
+  json.membership_limit @room.membership_limit
+  json.name @room.name
+  json.open_to_guests @room.open_to_guests?
+  json.topic @room.topic
+  json.updated_at @room.updated_at
+end
--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -15,7 +15,7 @@ describe "Room Requests" do
             "created_at" => room_1.created_at.as_json,
             "id" => room_1.id,
             "locked" => room_1.locked?,
-            "membership_limit" => 1_000_000,
+            "membership_limit" => room_1.membership_limit,
             "name" => room_1.name,
             "topic" => room_1.topic,
             "updated_at" => room_1.updated_at.as_json
@@ -24,7 +24,7 @@ describe "Room Requests" do
             "created_at" => room_2.created_at.as_json,
             "id" => room_2.id,
             "locked" => room_2.locked?,
-            "membership_limit" => 1_000_000,
+            "membership_limit" => room_2.membership_limit,
             "name" => room_2.name,
             "topic" => room_2.topic,
             "updated_at" => room_2.updated_at.as_json
@@ -39,7 +39,27 @@ describe "Room Requests" do
   end
 
   describe "GET /room/:id" do
-    pending
+    it "shows the room" do
+      room = create(:room, :with_guest_access, :locked)
+
+      get "/room/#{room.id}.json"
+
+      expect(response.status).to eq(200)
+      expect(response.json).to eq(
+        "room" => {
+          "active_token_value" => room.active_token_value,
+          "created_at" => room.created_at.as_json,
+          "full" => room.full?,
+          "id" => room.id,
+          "locked" => room.locked?,
+          "membership_limit" => room.membership_limit,
+          "name" => room.name,
+          "open_to_guests" => room.open_to_guests?,
+          "topic" => room.topic,
+          "updated_at" => room.updated_at.as_json
+        }
+      )
+    end
   end
 
   describe "PUT /room/:id" do
