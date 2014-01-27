cf3de1a00ac625b043d57c019d49749faeac69ae
Steve Richert <steve.richert@gmail.com>
2014-01-27 08:32:50 -0500

    Require basic authentication for all API endpoints

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -7,83 +7,159 @@ describe "Message Requests" do
     let!(:new_message) { create(:message, room: room, created_at: 1.minute.ago) }
     let!(:other_message) { create(:message) }
 
-    it "lists messages old to new" do
-      get "/room/#{room.id}/recent.json"
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      expect(response.status).to eq(200)
-      expect(response.json).to eq(
-        "messages" => [
-          {
-            "body" => old_message.body,
-            "created_at" => old_message.created_at.as_json,
-            "id" => old_message.id,
-            "room_id" => old_message.room_id
-          },
-          {
-            "body" => new_message.body,
-            "created_at" => new_message.created_at.as_json,
-            "id" => new_message.id,
-            "room_id" => new_message.room_id
-          }
-        ]
-      )
+      before do
+        authenticate(user.api_auth_token)
+      end
+
+      it "lists messages old to new" do
+        get "/room/#{room.id}/recent.json"
+
+        expect(response.status).to eq(200)
+        expect(response.json).to eq(
+          "messages" => [
+            {
+              "body" => old_message.body,
+              "created_at" => old_message.created_at.as_json,
+              "id" => old_message.id,
+              "room_id" => old_message.room_id
+            },
+            {
+              "body" => new_message.body,
+              "created_at" => new_message.created_at.as_json,
+              "id" => new_message.id,
+              "room_id" => new_message.room_id
+            }
+          ]
+        )
+      end
+    end
+
+    context "when unauthenticated" do
+      it "requires authentication" do
+        get "/room/#{room.id}/recent.json"
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 
   describe "POST /room/:room_id/speak" do
     let!(:room) { create(:room) }
 
-    it "creates a message" do
-      expect {
-        post_json "/room/#{room.id}/speak.json", %({"body":"Hello, world!"})
-      }.to change {
-        Message.count
-      }.from(0).to(1)
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      message = Message.last
+      before do
+        authenticate(user.api_auth_token)
+      end
 
-      expect(message.body).to eq("Hello, world!")
-      expect(message.room_id).to eq(room.id)
+      it "creates a message" do
+        expect {
+          post "/room/#{room.id}/speak.json", %({"body":"Hello, world!"})
+        }.to change {
+          Message.count
+        }.from(0).to(1)
 
-      expect(response.status).to eq(201)
-      expect(response.json).to eq(
-        "message" => {
-          "body" => message.body,
-          "created_at" => message.created_at.as_json,
-          "id" => message.id,
-          "room_id" => message.room_id
+        message = Message.last
+
+        expect(message.body).to eq("Hello, world!")
+        expect(message.room_id).to eq(room.id)
+
+        expect(response.status).to eq(201)
+        expect(response.json).to eq(
+          "message" => {
+            "body" => message.body,
+            "created_at" => message.created_at.as_json,
+            "id" => message.id,
+            "room_id" => message.room_id
+          }
+        )
+      end
+    end
+
+    context "when unauthenticated" do
+      it "requires authentication" do
+        expect {
+          post "/room/#{room.id}/speak.json", %({"body":"Hello, world!"})
+        }.not_to change {
+          Message.count
         }
-      )
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 
   describe "POST /messages/:id/star" do
     let!(:message) { create(:message) }
 
-    it "stars the message" do
-      expect {
-        post "/messages/#{message.id}/star.json"
-      }.to change {
-        message.reload.starred?
-      }.from(false).to(true)
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      expect(response.status).to eq(200)
-      expect(response.body).to be_blank
+      before do
+        authenticate(user.api_auth_token)
+      end
+
+      it "stars the message" do
+        expect {
+          post "/messages/#{message.id}/star.json"
+        }.to change {
+          message.reload.starred?
+        }.from(false).to(true)
+
+        expect(response.status).to eq(200)
+        expect(response.body).to be_blank
+      end
+    end
+
+    context "when unauthenticated" do
+      it "requires authentication" do
+        expect {
+          post "/messages/#{message.id}/star.json"
+        }.not_to change {
+          message.reload.starred?
+        }
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 
   describe "DELETE /messages/:id/star" do
     let!(:message) { create(:message, :starred) }
 
-    it "unstars the message" do
-      expect {
-        delete "/messages/#{message.id}/star.json"
-      }.to change {
-        message.reload.starred?
-      }.from(true).to(false)
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      expect(response.status).to eq(200)
-      expect(response.body).to be_blank
+      before do
+        authenticate(user.api_auth_token)
+      end
+
+      it "unstars the message" do
+        expect {
+          delete "/messages/#{message.id}/star.json"
+        }.to change {
+          message.reload.starred?
+        }.from(true).to(false)
+
+        expect(response.status).to eq(200)
+        expect(response.body).to be_blank
+      end
+    end
+
+    context "when unauthenticated" do
+      it "requires authentication" do
+        expect {
+          post "/messages/#{message.id}/star.json"
+        }.not_to change {
+          message.reload.starred?
+        }
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 end
--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -5,32 +5,48 @@ describe "Room Requests" do
     let!(:room_1) { create(:room, created_at: 2.days.ago) }
     let!(:room_2) { create(:room, :locked, created_at: 1.day.ago) }
 
-    it "lists all rooms" do
-      get "/rooms.json"
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      expect(response.status).to eq(200)
-      expect(response.json).to eq(
-        "rooms" => [
-          {
-            "created_at" => room_1.created_at.as_json,
-            "id" => room_1.id,
-            "locked" => room_1.locked?,
-            "membership_limit" => room_1.membership_limit,
-            "name" => room_1.name,
-            "topic" => room_1.topic,
-            "updated_at" => room_1.updated_at.as_json
-          },
-          {
-            "created_at" => room_2.created_at.as_json,
-            "id" => room_2.id,
-            "locked" => room_2.locked?,
-            "membership_limit" => room_2.membership_limit,
-            "name" => room_2.name,
-            "topic" => room_2.topic,
-            "updated_at" => room_2.updated_at.as_json
-          }
-        ]
-      )
+      before do
+        authenticate(user.api_auth_token)
+      end
+
+      it "lists all rooms" do
+        get "/rooms.json"
+
+        expect(response.status).to eq(200)
+        expect(response.json).to eq(
+          "rooms" => [
+            {
+              "created_at" => room_1.created_at.as_json,
+              "id" => room_1.id,
+              "locked" => room_1.locked?,
+              "membership_limit" => room_1.membership_limit,
+              "name" => room_1.name,
+              "topic" => room_1.topic,
+              "updated_at" => room_1.updated_at.as_json
+            },
+            {
+              "created_at" => room_2.created_at.as_json,
+              "id" => room_2.id,
+              "locked" => room_2.locked?,
+              "membership_limit" => room_2.membership_limit,
+              "name" => room_2.name,
+              "topic" => room_2.topic,
+              "updated_at" => room_2.updated_at.as_json
+            }
+          ]
+        )
+      end
+    end
+
+    context "when unauthenticated" do
+      it "requires authentication" do
+        get "/rooms.json"
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 
@@ -41,39 +57,75 @@ describe "Room Requests" do
   describe "GET /room/:id" do
     let!(:room) { create(:room, :with_guest_access, :locked) }
 
-    it "shows the room" do
-      get "/room/#{room.id}.json"
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      expect(response.status).to eq(200)
-      expect(response.json).to eq(
-        "room" => {
-          "active_token_value" => room.active_token_value,
-          "created_at" => room.created_at.as_json,
-          "full" => room.full?,
-          "id" => room.id,
-          "locked" => room.locked?,
-          "membership_limit" => room.membership_limit,
-          "name" => room.name,
-          "open_to_guests" => room.open_to_guests?,
-          "topic" => room.topic,
-          "updated_at" => room.updated_at.as_json
-        }
-      )
+      before do
+        authenticate(user.api_auth_token)
+      end
+
+      it "shows the room" do
+        get "/room/#{room.id}.json"
+
+        expect(response.status).to eq(200)
+        expect(response.json).to eq(
+          "room" => {
+            "active_token_value" => room.active_token_value,
+            "created_at" => room.created_at.as_json,
+            "full" => room.full?,
+            "id" => room.id,
+            "locked" => room.locked?,
+            "membership_limit" => room.membership_limit,
+            "name" => room.name,
+            "open_to_guests" => room.open_to_guests?,
+            "topic" => room.topic,
+            "updated_at" => room.updated_at.as_json
+          }
+        )
+      end
+    end
+
+    context "when unauthenticated" do
+      it "requires authentication" do
+        get "/room/#{room.id}.json"
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 
   describe "PUT /room/:id" do
     let!(:room) { create(:room, topic: "Hello!") }
 
-    it "updates the room" do
-      expect {
-        put_json "/room/#{room.id}.json", %({"topic":"Goodbye."})
-      }.to change {
-        room.reload.topic
-      }.from("Hello!").to("Goodbye.")
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      expect(response.status).to eq(200)
-      expect(response.body).to be_blank
+      before do
+        authenticate(user.api_auth_token)
+      end
+
+      it "updates the room" do
+        expect {
+          put "/room/#{room.id}.json", %({"topic":"Goodbye."})
+        }.to change {
+          room.reload.topic
+        }.from("Hello!").to("Goodbye.")
+
+        expect(response.status).to eq(200)
+        expect(response.body).to be_blank
+      end
+    end
+
+    context "when unauthenticated" do
+      it "requires authentication" do
+        expect {
+          put "/room/#{room.id}.json", %({"topic":"Goodbye."})
+        }.not_to change {
+          room.reload.topic
+        }
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 
@@ -88,38 +140,78 @@ describe "Room Requests" do
   describe "POST /room/:id/lock" do
     let!(:room) { create(:room) }
 
-    it "locks the room" do
-      expect {
-        post "/room/#{room.id}/lock.json"
-      }.to change {
-        room.reload.locked?
-      }.from(false).to(true)
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      expect(response.status).to eq(200)
-      expect(response.body).to be_blank
+      before do
+        authenticate(user.api_auth_token)
+      end
+
+      it "locks the room" do
+        expect {
+          post "/room/#{room.id}/lock.json"
+        }.to change {
+          room.reload.locked?
+        }.from(false).to(true)
+
+        expect(response.status).to eq(200)
+        expect(response.body).to be_blank
+      end
+
+      it "pauses transcripts" do
+        pending
+      end
     end
 
-    it "pauses transcripts" do
-      pending
+    context "when unauthenticated" do
+      it "requires authentication" do
+        expect {
+          post "/room/#{room.id}/lock.json"
+        }.not_to change {
+          room.reload.locked?
+        }
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 
   describe "POST /room/:id/unlock" do
     let!(:room) { create(:room, :locked) }
 
-    it "unlocks the room" do
-      expect {
-        post "/room/#{room.id}/unlock.json"
-      }.to change {
-        room.reload.locked?
-      }.from(true).to(false)
+    context "when authenticated" do
+      let!(:user) { create(:user) }
 
-      expect(response.status).to eq(200)
-      expect(response.body).to be_blank
+      before do
+        authenticate(user.api_auth_token)
+      end
+
+      it "unlocks the room" do
+        expect {
+          post "/room/#{room.id}/unlock.json"
+        }.to change {
+          room.reload.locked?
+        }.from(true).to(false)
+
+        expect(response.status).to eq(200)
+        expect(response.body).to be_blank
+      end
+
+      it "resumes transcripts" do
+        pending
+      end
     end
 
-    it "resumes transcripts" do
-      pending
+    context "when unauthenticated" do
+      it "requires authentication" do
+        expect {
+          post "/room/#{room.id}/unlock.json"
+        }.not_to change {
+          room.reload.locked?
+        }
+
+        expect(response.status).to eq(401)
+      end
     end
   end
 end
