ba8b4a125a5e521fc8304aee9a93cb5753fe3251
Steve Richert <steve.richert@gmail.com>
2014-01-30 07:22:36 -0500

    Lock down upload validations

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,6 +4,9 @@ class Upload < ActiveRecord::Base
 
   has_attached_file :file, url: "/files/:fingerprint.:extension"
 
+  validates :user_id, :room_id, presence: true, strict: true
+  validates_attachment :file, presence: true, size: { in: 0..10.megabytes }
+
   def self.old_to_new
     order(:created_at)
   end
