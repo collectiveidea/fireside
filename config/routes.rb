e3e668d1a089981f5247235ccf037f0ea2b6620e
Steve Richert <steve.richert@gmail.com>
2014-02-01 18:16:42 -0500

    Open up the streaming endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Fireside::Application.routes.draw do
   get  "room/:room_id/recent" => "messages#index"
   post "room/:room_id/speak"  => "messages#create"
 
+  get "room/:room_id/live" => "live_messages#index"
+
   post   "messages/:id/star" => "messages#star"
   delete "messages/:id/star" => "messages#unstar"
 
