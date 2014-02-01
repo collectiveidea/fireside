5c70db3a0ca16dceb3ef74b786b94052f6796991
Steve Richert <steve.richert@gmail.com>
2014-02-01 18:15:03 -0500

    Add the ability for rooms to subscribe to new messages
    …and receive small message payloads over PostgreSQL notify/subscribe.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,30 @@
 class Message < ActiveRecord::Base
+  class Payload < Struct.new(:attributes)
+    def self.load(payload_string)
+      new(JSON.load(payload_string))
+    end
+
+    def self.dump(message)
+      new(message.attributes).dump
+    end
+
+    def dump
+      JSON.dump(attributes)
+    end
+
+    private
+
+    def method_missing(method, *)
+      attributes.fetch(method.to_s) { super }
+    end
+  end
+
   belongs_to :user, inverse_of: :messages
   belongs_to :room, inverse_of: :messages
 
   validates :body, presence: true
+
+  after_create :notify_room
 
   def self.old_to_new
     order(:created_at)
@@ -34,5 +56,20 @@ class Message < ActiveRecord::Base
 
   def unstar
     update(starred: false)
+  end
+
+  private
+
+  def notify_room
+    connection = self.class.connection
+    connection.execute("NOTIFY #{channel}, #{connection.quote(payload_string)}")
+  end
+
+  def channel
+    "room_#{room_id}"
+  end
+
+  def payload_string
+    Payload.dump(self)
   end
 end
