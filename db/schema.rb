6352a97719aafece81c045777acddbd1a4c8eeec
Steve Richert <steve.richert@gmail.com>
2014-02-02 07:38:14 -0500

    Remove Paperclip's attachment timestamp column
    Once uploaded, files won't change.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130121154) do
+ActiveRecord::Schema.define(version: 20140202122025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 20140130121154) do
     t.string   "file_file_name"
     t.string   "file_content_type"
     t.integer  "file_file_size"
-    t.datetime "file_updated_at"
     t.string   "file_fingerprint"
   end
 
