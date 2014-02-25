84bec5f0a16ccf14264abf676c937390ada4dd2c
Steve Richert <steve.richert@gmail.com>
2014-02-25 16:13:06 -0500

    Name the rooms resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Fireside::Application.routes.draw do
   get "rooms"    => "rooms#index",   as: nil
   get "presence" => "rooms#current", as: nil
 
-  get  "room/:id"        => "rooms#show"
+  get  "room/:id"        => "rooms#show",   as: :room
   put  "room/:id"        => "rooms#update"
-  post "room/:id/join"   => "rooms#join"
-  post "room/:id/leave"  => "rooms#leave"
-  post "room/:id/lock"   => "rooms#lock"
-  post "room/:id/unlock" => "rooms#unlock"
+  post "room/:id/join"   => "rooms#join",   as: :join_room
+  post "room/:id/leave"  => "rooms#leave",  as: :leave_room
+  post "room/:id/lock"   => "rooms#lock",   as: :lock_room
+  post "room/:id/unlock" => "rooms#unlock", as: :unlock_room
 
   get "room/:room_id/recent"                       => "messages#index"
   get "room/:room_id/transcript"                   => "messages#today"
